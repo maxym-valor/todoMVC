b63fc7cdb3627698f2349d4681c9763c11756713
maxym-valor <maxym.riabchuk@valor-software.com>
2022-11-01 16:15:54 +0200

    covert TodoTextInput.js to functional component

--- src/components/TodoTextInput.js
+++ src/components/TodoTextInput.js
@@ -1,54 +1,44 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import classnames from "classnames";
 
-export default class TodoTextInput extends Component {
-  static propTypes = {
-    onSave: PropTypes.func.isRequired,
-    text: PropTypes.string,
-    placeholder: PropTypes.string,
-    editing: PropTypes.bool,
-    newTodo: PropTypes.bool,
-  };
+function TodoTextInput({ onSave, text, placeholder, editing, newTodo }) {
+  const [currentText, setText] = useState(text || "");
 
-  state = {
-    text: this.props.text || "",
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     const text = e.target.value.trim();
     if (e.which === 13) {
-      this.props.onSave(text);
-      if (this.props.newTodo) {
-        this.setState({ text: "" });
+      onSave(text);
+      if (newTodo) {
+        setText("");
       }
     }
   };
 
-  handleChange = (e) => {
-    this.setState({ text: e.target.value });
+  const handleChange = (e) => {
+    setText(e.target.value);
   };
 
-  handleBlur = (e) => {
-    if (!this.props.newTodo) {
-      this.props.onSave(e.target.value);
+  const handleBlur = (e) => {
+    if (!newTodo) {
+      onSave(e.target.value);
     }
   };
-  render() {
-    return (
-      <input
-        className={classnames({
-          edit: this.props.editing,
-          "new-todo": this.props.newTodo,
-        })}
-        type="text"
-        placeholder={this.props.placeholder}
-        autoFocus="true"
-        value={this.state.text}
-        onBlur={this.handleBlur}
-        onChange={this.handleChange}
-        onKeyDown={this.handleSubmit}
-      />
-    );
-  }
+
+  return (
+    <input
+      className={classnames({
+        edit: editing,
+        "new-todo": newTodo,
+      })}
+      type="text"
+      placeholder={placeholder}
+      autoFocus="true"
+      value={currentText}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      onKeyDown={handleSubmit}
+    />
+  );
 }
+
+export default TodoTextInput;
